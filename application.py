8f5d6fcd4c60bc10879549e4a54b6a2d0aa700ec
Pedro Alves F <palves1945@gmail.com>
2019-08-29 09:44:07 -0300

    2019-08-29 09:44

--- application.py
+++ application.py
@@ -9,6 +9,7 @@ from catalog.populate_db import populate
 if __name__ == '__main__':
     # App configuration
     app.config['DATABASE_URL'] = 'sqlite:///itemcatalog.db'
+    # Or for PostgreSQL:
     #app.config['DATABASE_URL'] = 'postgresql://catalog:PASSWORD@localhost/catalog'    
     app.config['OAUTH_SECRETS_LOCATION'] = ''    
     app.secret_key = 'super_secret_key'  # This needs changing in production env
--- catalog/connect_to_database.py
+++ catalog/connect_to_database.py
@@ -7,6 +7,9 @@ from catalog.database_setup import Base
 
 def connect_to_database():
     """Connects to the database and returns an sqlalchemy session object."""
+    # 'postgresql://catalog:PASSWORD@localhost/catalog'
+    # DATABASE_URL = 'postgresql://catalog:PASSWORD@localhost/catalog'
+    # app.config['DATABASE_URL'] defined in `application.py`
     engine = create_engine(app.config['DATABASE_URL'])
     Base.metadata.bind = engine
     db_session = sessionmaker(bind=engine)
